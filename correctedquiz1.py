67974fede6ff01746c15e9345436a9db5cae9eb6
Stubbycat85 <stusam07@ccm-2610-b1.evergreen.edu>
2013-10-17 15:43:12 -0700

    Message in there

---
+++ correctedquiz1.py
@@ -0,0 +1,10 @@
+# Grader Name - Hunter Crook
+# Grader Login - crohun22
+import lab2
+n = lab2.n
+while (n > 0):
+    n = n-1
+    print "Hello World!"
+print "Yay! Graded your world?"
+
+
---
+++ quiz1.py
@@ -0,0 +1,9 @@
+# Grader Name - Hunter Crook
+# Grader Login - crohun22
+import lab2
+n = lab2.n
+while (n > 0):
+    n = n-1
+    print "Hello World!"
+print "Yay! Graded your world?"
+
